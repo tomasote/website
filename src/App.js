a143646368c6c534a7b3bc743c5d46b9579feef0
marwinhormiz <marwinhormiz@gmail.com>
2022-11-17 00:19:49 +0100

    test v2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function Parse(props){
     transform: isInView ? "none" : "translateX(-200px)",
     opacity: isInView ? 1 : 0,
     transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-  }} className='welcome'>{text}</motion.p>;
+  }} className='welcome'>{text ? text : "TEXT LOADING ERROR"}</motion.p>;
 }
 
 function Test(props){
