a743c091b8763f8d362f7b7a001bd1057ec4202a
marwinhormiz <marwinhormiz@gmail.com>
2022-11-18 22:23:58 +0100

    url fix

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function Header2(props){
 }
 
 function ParsePic(props){
-  const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000/" : "https://www.tomasperez.se/"
+  const baseUrl = document.URL //process.env.NODE_ENV === 'development' ? "http://localhost:3000/" : "https://www.tomasperez.se/"
   const [text, setText] = useState(null);
   useEffect(()=>{
     fetch(baseUrl+props.file, {
@@ -52,7 +52,7 @@ function Parse(props){
   const isInView = useInView(ref, { once: true });
   const [text, setText] = useState("");
   useEffect(()=>{
-    const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000/" : "https://www.tomasperez.se/"
+    const baseUrl = document.URL //process.env.NODE_ENV === 'development' ? "http://localhost:3000/" : "https://www.tomasperez.se/"
     fetch(baseUrl+props.file, {
       mode: 'same-origin',
       credentials: 'omit',
